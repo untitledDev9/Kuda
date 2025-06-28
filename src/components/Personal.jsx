b0911dd9d7ff13568b672d98d296122d8c2930c3
untitled <ahmedsuleiman940@gmail.com>
2025-06-28 02:01:53 +0100

    done wit business modal

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -22,9 +22,9 @@ import { TbMoneybag } from "react-icons/tb";
 const Personal = () => {
   const [drop, setDrop] = useState(false)
   return (
-    <div 
-    onMouseEnter={() => setDrop(true)}
-    onMouseLeave={() => setDrop(false)}
+    <div
+      onMouseEnter={() => setDrop(true)}
+      onMouseLeave={() => setDrop(false)}
     >
       <div className=' flex items-center justify-center gap-1 relative cursor-pointer font-[500] text-[#40196D]'>
         <p>Personnal</p>
@@ -33,104 +33,104 @@ const Personal = () => {
 
       {drop && (
         <div className=' absolute '>
-                 <div className=' flex bg-white w-[100%] '>
-      <div className=' flex flex-col px-10 gap-5 py-7'>
-        
-        <PayBills 
-        iconBg=""
-        icon={<IoMdHome />}
-        title="Discover Personal"
-        />
+          <div className=' flex bg-white w-[100%] '>
+            <div className=' flex flex-col px-10 gap-5 py-7'>
 
-        <PayBills 
-        iconBg=""
-        icon={<RiTelegram2Fill />}
-        title="Transfer & Spend"
-        />
+              <PayBills
+                iconBg=""
+                icon={<IoMdHome />}
+                title="Discover Personal"
+              />
 
-        <PayBills 
-        iconBg=""
-        icon={<HiSaveAs />}
-        title=" Save"
-        />
+              <PayBills
+                iconBg=""
+                icon={<RiTelegram2Fill />}
+                title="Transfer & Spend"
+              />
 
-        <PayBills 
-        iconBg=""
-        icon={<GiNetworkBars />}
-        title="Investments"
-        />
+              <PayBills
+                iconBg=""
+                icon={<HiSaveAs />}
+                title=" Save"
+              />
 
-        <PayBills 
-        iconBg=""
-        icon={<FaCreditCard />}
-        title="Kuda Card"
-        />
-      </div>
-      <div className='  bg-[#b8b8b831] px-10 py-7 flex flex-col gap-5'>
-        <h1 className=' text-[#505050]'>PAYMENTS</h1>
-        
-        <PayBills 
-        iconBg=""
-        icon={<BsLightningFill />}
-        title="Electricity"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<FaMobileAlt />}
-        title="Airtime"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<FaWifi />}
-        title="Internet"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<IoMdGift />}
-        title="Gift Cards"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<TbWorldDollar />}
-        title="Cardless Payments"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<MdLiveTv />}
-        title="TV"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<TbArrowBigUpFilled />}
-        title="Betting"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<FaCar />}
-        title="Transport"
-        />
-        <h1 className=' text-[#505050]'>CREDIT</h1>
-        
-        <PayBills 
-        iconBg=""
-        icon={<VscIssueDraft />}
-        title="Overdrafts"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<TbMoneybag />}
-        title="Loans"
-        />
-      </div>
-    </div>
+              <PayBills
+                iconBg=""
+                icon={<GiNetworkBars />}
+                title="Investments"
+              />
+
+              <PayBills
+                iconBg=""
+                icon={<FaCreditCard />}
+                title="Kuda Card"
+              />
+            </div>
+            <div className='  bg-[#b8b8b831] px-10 py-7 flex flex-col gap-5'>
+              <h1 className=' text-[#505050]'>PAYMENTS</h1>
+
+              <PayBills
+                iconBg=""
+                icon={<BsLightningFill />}
+                title="Electricity"
+              />
+
+              <PayBills
+                iconBg=""
+                icon={<FaMobileAlt />}
+                title="Airtime"
+              />
+
+              <PayBills
+                iconBg=""
+                icon={<FaWifi />}
+                title="Internet"
+              />
+
+              <PayBills
+                iconBg=""
+                icon={<IoMdGift />}
+                title="Gift Cards"
+              />
+
+              <PayBills
+                iconBg=""
+                icon={<TbWorldDollar />}
+                title="Cardless Payments"
+              />
+
+              <PayBills
+                iconBg=""
+                icon={<MdLiveTv />}
+                title="TV"
+              />
+
+              <PayBills
+                iconBg=""
+                icon={<TbArrowBigUpFilled />}
+                title="Betting"
+              />
+
+              <PayBills
+                iconBg=""
+                icon={<FaCar />}
+                title="Transport"
+              />
+              <h1 className=' text-[#505050]'>CREDIT</h1>
+
+              <PayBills
+                iconBg=""
+                icon={<VscIssueDraft />}
+                title="Overdrafts"
+              />
+
+              <PayBills
+                iconBg=""
+                icon={<TbMoneybag />}
+                title="Loans"
+              />
+            </div>
+          </div>
         </div>
       )}
     </div>
--- src/resueable/BigDropDown.jsx
+++ src/resueable/BigDropDown.jsx
@@ -16,107 +16,273 @@ import { TbArrowBigUpFilled } from "react-icons/tb";
 import { FaCar } from "react-icons/fa";
 import { VscIssueDraft } from "react-icons/vsc";
 import { TbMoneybag } from "react-icons/tb";
+import { FaRegMoneyBillAlt } from "react-icons/fa";
+
+
+
+
+
+
+
+
+
+import { IoIosPeople } from "react-icons/io";
+import { SiBento } from "react-icons/si";
+import { RiFolderOpenFill } from "react-icons/ri";
+import { GiArchiveRegister } from "react-icons/gi";
+import { BsPhone } from "react-icons/bs";
+import { TbDeviceLandlinePhone } from "react-icons/tb";
+import { MdOutlineMobileOff } from "react-icons/md";
+import { FaFileInvoiceDollar } from "react-icons/fa6";
 
 
 const BigDropDown = () => {
   return (
-    <div className=' flex bg-white w-[100%] '>
-      <div className=' flex flex-col px-10 gap-5 py-7'>
-        
-        <PayBills 
-        iconBg=""
-        icon={<IoMdHome />}
-        title="Discover Personal"
-        />
+    <div className=' flex'>
+      <div className=' flex bg-white w-[100%] '>
+        <div className=' flex flex-col px-10 gap-5 py-7'>
 
-        <PayBills 
-        iconBg=""
-        icon={<RiTelegram2Fill />}
-        title="Transfer & Spend"
-        />
+          <PayBills
+            iconBg=""
+            icon={<IoMdHome />}
+            title="Discover Personal"
+          />
 
-        <PayBills 
-        iconBg=""
-        icon={<HiSaveAs />}
-        title=" Save"
-        />
+          <PayBills
+            iconBg=""
+            icon={<RiTelegram2Fill />}
+            title="Transfer & Spend"
+          />
 
-        <PayBills 
-        iconBg=""
-        icon={<GiNetworkBars />}
-        title="Investments"
-        />
+          <PayBills
+            iconBg=""
+            icon={<HiSaveAs />}
+            title=" Save"
+          />
 
-        <PayBills 
-        iconBg=""
-        icon={<FaCreditCard />}
-        title="Kuda Card"
-        />
+          <PayBills
+            iconBg=""
+            icon={<GiNetworkBars />}
+            title="Investments"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<FaCreditCard />}
+            title="Kuda Card"
+          />
+        </div>
+        <div className='  bg-[#8a8a8a5b] px-10 py-7 flex flex-col gap-5'>
+          <h1 className=' text-[#505050]'>PAYMENTS</h1>
+
+          <PayBills
+            iconBg=""
+            icon={<BsLightningFill />}
+            title="Electricity"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<FaMobileAlt />}
+            title="Airtime"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<FaWifi />}
+            title="Internet"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<IoMdGift />}
+            title="Gift Cards"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<TbWorldDollar />}
+            title="Cardless Payments"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<MdLiveTv />}
+            title="TV"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<TbArrowBigUpFilled />}
+            title="Betting"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<FaCar />}
+            title="Transport"
+          />
+          <h1 className=' text-[#505050]'>CREDIT</h1>
+
+          <PayBills
+            iconBg=""
+            icon={<VscIssueDraft />}
+            title="Overdrafts"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<TbMoneybag />}
+            title="Loans"
+          />
+        </div>
       </div>
-      <div className='  bg-[#8a8a8a5b] px-10 py-7 flex flex-col gap-5'>
-        <h1 className=' text-[#505050]'>PAYMENTS</h1>
-        
-        <PayBills 
-        iconBg=""
-        icon={<BsLightningFill />}
-        title="Electricity"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<FaMobileAlt />}
-        title="Airtime"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<FaWifi />}
-        title="Internet"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<IoMdGift />}
-        title="Gift Cards"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<TbWorldDollar />}
-        title="Cardless Payments"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<MdLiveTv />}
-        title="TV"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<TbArrowBigUpFilled />}
-        title="Betting"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<FaCar />}
-        title="Transport"
-        />
-        <h1 className=' text-[#505050]'>CREDIT</h1>
-        
-        <PayBills 
-        iconBg=""
-        icon={<VscIssueDraft />}
-        title="Overdrafts"
-        />
-        
-        <PayBills 
-        iconBg=""
-        icon={<TbMoneybag />}
-        title="Loans"
-        />
+
+
+
+
+
+      <div className=' flex bg-white w-[100%] '>
+        <div className=' flex flex-col px-10 gap-5 py-7'>
+
+          <PayBills
+            iconBg=""
+            icon={<IoMdHome />}
+            title="Discover Business"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<GiArchiveRegister />}
+            title="Business Registration"
+          />
+
+          <PayBills
+            iconBg=""
+            icon='K.'
+            title="Kuda Payroll"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<RiFolderOpenFill />}
+            title="Business API"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<SiBento />}
+            title="Payroll with Bento"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<IoIosPeople />}
+            title="Expense Accounts"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<FaCreditCard />}
+            title="Expense Card"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<IoMdHome />}
+            title="Discover Business"
+          />
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+        <div className='  bg-[#8a8a8a5b] px-10 py-7 flex flex-col gap-5'>
+          <h1 className=' text-[#505050]'>PAYMENTS</h1>
+
+          <PayBills
+            iconBg=""
+            icon={<RiTelegram2Fill />}
+            title="Send Money"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<MdLiveTv />}
+            title="TV"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<FaMobileAlt />}
+            title="Airtime & Internet Data"
+          />
+
+
+          <PayBills
+            iconBg=""
+            icon={<BsLightningFill />}
+            title="Electricity"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<FaRegMoneyBillAlt />}
+            title="Bill Payments"
+          />
+
+          <h1 className=' text-[#505050]'>CREDIT</h1>
+          <PayBills
+            iconBg=""
+            icon={<TbMoneybag />}
+            title="Businedd Loan"
+          />
+
+
+
+          <h1 className=' text-[#505050]'>COLLECTIONS</h1>
+
+          <PayBills
+            iconBg=""
+            icon={<TbDeviceLandlinePhone />}
+            title="softPOS"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<FaFileInvoiceDollar />}
+            title="Invoicing for Businedd"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<BsPhone />}
+            title="POS Machine"
+          />
+
+          <PayBills
+            iconBg=""
+            icon={<MdOutlineMobileOff />}
+            title="Virtual POS"
+          />
+
+
+        </div>
       </div>
+
     </div>
   )
 }
